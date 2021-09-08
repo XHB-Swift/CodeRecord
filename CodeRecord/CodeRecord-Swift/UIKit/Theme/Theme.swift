3460f7045035eca25fd722177575ef05409024ec
谢鸿标 <1021580211@qq.com>
2021-09-08 23:36:23 +0800

    keypath更换主题

--- CodeRecord/CodeRecord-Swift/UIKit/Theme/Theme.swift
+++ CodeRecord/CodeRecord-Swift/UIKit/Theme/Theme.swift
@@ -14,59 +14,77 @@ extension String {
     public static let light = "light"
 }
 
-open class Theme {
+public protocol ThemeAttribute {
     
-    //表示该主题针对view的哪个属性
-    open var property: String
+    associatedtype Attribute
     
-    open var color: String?
-    open var colorAlpha: CGFloat?
+    func toAttribute() -> Attribute?
+}
+
+public protocol ThemeUpdatable: AnyObject {
     
-    open var image: String?
+    func update<K: ThemeUpdatable, S: ThemeAttribute>(style: S, for property: PartialKeyPath<K>)
+}
+
+public struct Color: ThemeAttribute {
     
-    open var fontName: String?
-    open var fontSize: CGFloat?
+    public typealias Attribute = UIColor
     
-    public init(property: String) {
-        self.property = property
-    }
+    public var color: String?
+    public var alpha: CGFloat?
     
-    open var toRealColor: UIColor? {
+    public func toAttribute() -> UIColor? {
         guard let colorStr = color else { return nil }
-        return UIColor(hexString: colorStr, alpha: colorAlpha ?? 1)
+        return UIColor(hexString: colorStr, alpha: alpha ?? 1)
     }
+}
+
+public struct Font: ThemeAttribute {
     
-    open var toFont: UIFont? {
+    public typealias Attribute = UIFont
+    
+    public var fontName: String?
+    public var fontSize: CGFloat?
+    
+    public func toAttribute() -> UIFont? {
         guard let name = fontName, let size = fontSize else { return nil }
         return UIFont(name: name, size: size)
     }
 }
 
-public protocol ThemeUpdatable: AnyObject {
+open class Theme<K: ThemeUpdatable, S: ThemeAttribute> {
     
-    func update(theme: Theme)
+    //表示该主题针对view的哪个属性
+    open var property: PartialKeyPath<K>
+    
+    open var style: S
+    
+    public init(property: PartialKeyPath<K>, style: S) {
+        self.property = property
+        self.style = style
+    }
 }
 
-fileprivate class ThemeObject {
+fileprivate class ThemeObject<K: ThemeUpdatable, S: ThemeAttribute> {
     
     public weak var view: ThemeUpdatable?
-    fileprivate var themeInfo = Dictionary<String, Theme>()
+    fileprivate var themeInfo = Dictionary<String, Theme<K, S>>()
 }
 
 open class ThemeManager {
     
     private init() {}
     public static let shared = ThemeManager()
-    private var themeObjects = Dictionary<String, ThemeObject>()
+    private var themeObjects = Dictionary<String, Any>()
     
-    open func set(theme: Theme, style: String, for view: ThemeUpdatable?) {
+    open func set<K: ThemeUpdatable, S: ThemeAttribute>(theme: Theme<K, S>, style: String, for view: ThemeUpdatable?) {
         guard let v = view else { return }
         
         let viewId = "\(v)"
-        if let themeObject = themeObjects[viewId] {
+        if let themeObject = themeObjects[viewId] as? ThemeObject<K,S> {
             themeObject.themeInfo[style] = theme
         }else {
-            let themeObject = ThemeObject()
+            let themeObject = ThemeObject<K,S>()
             themeObject.themeInfo[style] = theme
             themeObjects[viewId] = themeObject
         }
@@ -74,8 +92,11 @@ open class ThemeManager {
     
     open func switchTo(theme style: String) {
         _ = themeObjects.map { key, value in
-            guard let theme = value.themeInfo[style] else { return }
-            value.view?.update(theme: theme)
+            if let color = value as? ThemeObject<UIView, Color>,
+               let view = color.view,
+               let theme = color.themeInfo["\(view)"] {
+                view.update(style: theme.style, for: theme.property)
+            }
         }
     }
 }
@@ -90,12 +111,33 @@ extension String {
 extension UIView: ThemeUpdatable {
     
     open func theme_set(backgroundColor: String, for style: String) {
-        let theme = Theme(property: .backgroundColor)
-        theme.color = backgroundColor
+        let color = Color(color: backgroundColor, alpha: 1)
+        let theme = Theme(property: \UIView.backgroundColor, style:color)
         ThemeManager.shared.set(theme: theme, style: style, for: self)
     }
     
-    public func update(theme: Theme) {
-        backgroundColor = theme.toRealColor
+    public func update<K: ThemeUpdatable, S: ThemeAttribute>(style: S, for property: PartialKeyPath<K>) {
+        guard let writableKeyPath = property as? ReferenceWritableKeyPath<UIView, S> else { return }
+        if let colorKeyPath = writableKeyPath as? ReferenceWritableKeyPath<UIView, UIColor?> {
+            self[keyPath: colorKeyPath] = style.toAttribute() as? UIColor
+        }
+        if let fontKeyPath = writableKeyPath as? ReferenceWritableKeyPath<UIView, UIFont?> {
+            self[keyPath: fontKeyPath] = style.toAttribute() as? UIFont
+        }
+    }
+}
+
+extension UILabel {
+
+    open func theme_set(textColor: String, for style: String) {
+        let color = Color(color: textColor, alpha: 1)
+        let theme = Theme(property: \UILabel.textColor, style: color)
+        ThemeManager.shared.set(theme: theme, style: style, for: self)
+    }
+    
+    open func theme_set(font: UIFont, for style: String) {
+        let font1 = Font(fontName: font.fontName, fontSize: font.pointSize)
+        let theme = Theme(property: \UILabel.font, style: font1)
+        ThemeManager.shared.set(theme: theme, style: style, for: self)
     }
 }
