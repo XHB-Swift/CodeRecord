8475988883077a5141005371e9fa23c17ea761d0
谢鸿标 <xiehongbiao@corp.netease.com>
2022-06-20 11:13:51 +0800

    自定义气泡Demo完善

--- CodeRecord/CodeRecord-Swift/CustomBubbleTipsViewController.swift
+++ CodeRecord/CodeRecord-Swift/CustomBubbleTipsViewController.swift
@@ -38,17 +38,26 @@ class CustomBubbleTipsViewController: UIViewController {
         createUpArrowTips()
         createDownArrowTips()
         createLeftArrowTips()
-//        createRightArrowTips()
+        createRightArrowTips()
     }
     
-    func createLabelTips(position: CGPoint, tips: String, direction: BezierPathBubbleTipsView.ArrowDirection) {
+    func createLabelTips(tips: String,
+                         position: CGPoint,
+                         maxWidth: CGFloat,
+                         direction: BezierPathBubbleTipsView.ArrowDirection) {
         let label = UILabel()
         label.numberOfLines = 0
         let attrString: AttributedInterpotationString = """
-        This is an \(tips, .color("FFF0FFF0")) tips
+        This is an \(tips, .color("FFF0FFF0"), .font(UIFont(name: "PingFangSC-Medium", size: 25)!)) tips
         """
         label.attributedText = attrString.attributedString
-        label.sizeToFit()
+        label.size = attrString.attributedString.boundingRect(with: CGSize(width: maxWidth,
+                                                          height: view.height),
+                                             options: [
+                                                .usesLineFragmentOrigin,
+                                                .usesFontLeading,
+                                             ],
+                                             context: nil).size
         let bubbleTips = BezierPathBubbleTipsView(customView: label)
         bubbleTips.frame = view.bounds
         bubbleTips.point = position
@@ -61,22 +70,22 @@ class CustomBubbleTipsViewController: UIViewController {
     }
     
     func createUpArrowTips() {
-        let point = CGPoint(x: contentLabel.x, y: contentLabel.bottom + 4)
-        createLabelTips(position: point, tips: "up arrow", direction: .up)
+        let point = CGPoint(x: contentLabel.x + 20, y: contentLabel.bottom + 5)
+        createLabelTips(tips: "up arrow", position: point, maxWidth: 150, direction: .up)
     }
     
     func createDownArrowTips() {
-        let point = CGPoint(x: contentLabel.x, y: contentLabel.y - 4)
-        createLabelTips(position: point, tips: "down arrow", direction: .down)
+        let point = CGPoint(x: contentLabel.x + 20, y: contentLabel.y - 5)
+        createLabelTips(tips: "down arrow", position: point, maxWidth: 150, direction: .down)
     }
     
     func createLeftArrowTips() {
-        let point = CGPoint(x: contentLabel.right + 4, y: contentLabel.y + 4)
-        createLabelTips(position: point, tips: "left arrow", direction: .left)
+        let point = CGPoint(x: contentLabel.right + 20, y: contentLabel.y + 5)
+        createLabelTips(tips: "left arrow", position: point, maxWidth: 60, direction: .left)
     }
     
     func createRightArrowTips() {
-        let point = CGPoint(x: 0, y: contentLabel.y + 4)
-        createLabelTips(position: point, tips: "right arrow", direction: .right)
+        let point = CGPoint(x: contentLabel.x - 20, y: contentLabel.y + 5)
+        createLabelTips(tips: "right arrow", position: point, maxWidth: 60, direction: .right)
     }
 }
